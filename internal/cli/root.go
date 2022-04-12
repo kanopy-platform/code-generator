acbb60a8cf3512fed2235c775500ae3c85fac93c
David Katz <david.katz@mongodb.com>
2022-04-12 16:33:28 -0400

    clean up

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -2,6 +2,7 @@ package cli
 
 import (
 	"github.com/kanopy-platform/code-generator/pkg/generators"
+	log "github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	gengoargs "k8s.io/gengo/args"
@@ -29,36 +30,52 @@ func NewRootCommand(opts ...func(*rootCommand)) *cobra.Command {
 	rootCommand.GeneratorArgs.OutputFileBaseName = "zz_generated_builders"
 
 	cmd := &cobra.Command{
-		Use:   "kanopy-codegen",
-		Short: "Kanopy Builder code generator",
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-
-			if err := viper.BindPFlags(cmd.Flags()); err != nil {
-				return err
-			}
-
-			return nil
-		},
-		RunE: rootCommand.runE,
+		Use:               "kanopy-codegen",
+		Short:             "Kanopy Builder code generator",
+		PersistentPreRunE: rootCommand.prerun,
+		RunE:              rootCommand.runE,
 	}
 
-	flags := cmd.PersistentFlags()
-	customArgs := &generators.CustomArgs{}
-	flagLogLevel(flags)
-	flags.StringSliceVar(&customArgs.BoundingDirs, "bounding-dirs", customArgs.BoundingDirs, "specify directories to bound the generation")
-	flagGeneratorArgs(flags, rootCommand.GeneratorArgs)
-
-	rootCommand.GeneratorArgs.CustomArgs = customArgs
+	rootCommand.setupFlags(cmd)
 
 	return cmd
 }
 
+func (r *rootCommand) setupFlags(cmd *cobra.Command) {
+	flags := cmd.PersistentFlags()
+	flagLogLevel(flags)
+	flagGeneratorArgs(flags, r.GeneratorArgs)
+
+	customArgs := &generators.CustomArgs{}
+	flagCustomGeneratorArgs(flags, customArgs)
+	r.GeneratorArgs.CustomArgs = customArgs
+}
+
+func (r *rootCommand) prerun(cmd *cobra.Command, args []string) error {
+
+	if err := viper.BindPFlags(cmd.Flags()); err != nil {
+		return err
+	}
+
+	return setupGlobalLogLevel()
+}
+
 func (r *rootCommand) runE(cmd *cobra.Command, args []string) error {
-	// TODO add argument to override outputfilebasename from args
 
 	return r.GeneratorArgs.Execute(
 		generators.NameSystems(),
 		generators.DefaultNameSystem(),
 		generators.Packages,
 	)
+}
+
+func setupGlobalLogLevel() error {
+	// set log level
+	logLevel, err := log.ParseLevel(viper.GetString("log-level"))
+	if err != nil {
+		return err
+	}
+
+	log.SetLevel(logLevel)
+	return nil
 }
